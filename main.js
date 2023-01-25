3513243ed2789f7260bba271728e7a3646378e29
Mateo Falcon <neverxfulll@gmail.com>
2023-01-24 23:40:21 -0300

    Segunda versión

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 class Product {
-    constructor(title,description,price,thumbnail,code,stock){
+    
+    constructor(title,description,price,thumbnail,code,stock,id){
         this.title=title;
         this.description=description;
         this.price=price;
         this.thumbnail=thumbnail;
         this.code=code;
         this.stock=stock;
+        this.id=ProductManager.productId;
     }
   
 }
@@ -13,6 +15,7 @@ class Product {
 
 
 class ProductManager{
+    static productId = 0;
     constructor(){
          this.products = new Array();
     
@@ -20,9 +23,10 @@ class ProductManager{
     
   
 
-    addProduct = (title,description,price,thumbnail,code,stock) => {
-        let nuevoProducto = new Product(title,description,price,thumbnail,code,stock);
+    addProduct = (title,description,price,thumbnail,code,stock,id) => {   for(let i=0;i<this.products.length;i++){}
+        let nuevoProducto = new Product(title,description,price,thumbnail,code,stock,id);
         this.products.push(nuevoProducto);
+        ProductManager.productId++;
 
     }
 
@@ -31,13 +35,15 @@ class ProductManager{
     }
 
     
+    
+    
     getProductById = (id) =>{
         for(let i=0;i<this.products.length;i++){
             if(this.products.includes(id,i)){
                 console.log("Se ha encontrado un producto que coincide con ese id")
             }
             else{
-                console.error("Not found")
+                console.error("Not found");
             }
         }
     }
@@ -50,11 +56,20 @@ class ProductManager{
 
 let productManager = new ProductManager();
 console.log(productManager);
-productManager.addProduct("Manzana","manzana roja", 150, "https://static9.depositphotos.com/1642482/1149/i/600/depositphotos_11490598-stock-photo-red-apple-fruit.jpg", "1xlf", 5);
+productManager.addProduct("Manzana","Manzana roja", 150, "https://static9.depositphotos.com/1642482/1149/i/600/depositphotos_11490598-stock-photo-red-apple-fruit.jpg", "1xlf", 5);
+productManager.addProduct("Naranja","Naranaja sin semillas", 120, "https://www.lechepuleva.es/documents/13930/203222/naranja_g.jpg/374b25a1-2f66-4c7a-b7d1-d123cb310073?t=1423215361000", "1zlf", 4);
+productManager.addProduct("Banana","Banana ecuatoriana", 100, "https://img.freepik.com/foto-gratis/disparo-alto-angulo-platano-aislado-sobre-superficie-blanca_181624-29513.jpg?auto=format&h=200", "1ylf", 2);
+
 productManager.getProducts();
-productManager.getProductById();
+
+
+
+productManager.getProductById(1);
 
 console.log(productManager)
+
+
+
 
 
 
