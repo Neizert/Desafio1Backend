c579c039530453befd9f354e0394aa66456d3c67
Mateo Falcon <neverxfulll@gmail.com>
2023-01-25 00:28:46 -0300

    Cuarta versión

--- main.js
+++ main.js
@@ -23,10 +23,13 @@ class ProductManager{
     
   
 
-    addProduct = (title,description,price,thumbnail,code,stock,id) => {   
-        let nuevoProducto = new Product(title,description,price,thumbnail,code,stock,id);
+    addProduct = (title,description,price,thumbnail,code,stock,id) => { 
+        if(!this.products.find(i => i.code===code)){
+            let nuevoProducto = new Product(title,description,price,thumbnail,code,stock,id);
         this.products.push(nuevoProducto);
         ProductManager.productId++;
+
+        }
 
     }
 
@@ -58,12 +61,12 @@ let productManager = new ProductManager();
 console.log(productManager);
 productManager.addProduct("Manzana","Manzana roja", 150, "https://static9.depositphotos.com/1642482/1149/i/600/depositphotos_11490598-stock-photo-red-apple-fruit.jpg", "1xlf", 5);
 productManager.addProduct("Naranja","Naranaja sin semillas", 120, "https://www.lechepuleva.es/documents/13930/203222/naranja_g.jpg/374b25a1-2f66-4c7a-b7d1-d123cb310073?t=1423215361000", "1zlf", 4);
-productManager.addProduct("Banana","Banana ecuatoriana", 100, "https://img.freepik.com/foto-gratis/disparo-alto-angulo-platano-aislado-sobre-superficie-blanca_181624-29513.jpg?auto=format&h=200", "1ylf", 2);
+productManager.addProduct("Banana","Banana ecuatoriana", 100, "https://img.freepik.com/foto-gratis/disparo-alto-angulo-platano-aislado-sobre-superficie-blanca_181624-29513.jpg?auto=format&h=200", "1zlf", 2);
 
 productManager.getProducts();
 
 
-productManager.getProductById(2);
+productManager.getProductById(1);
 productManager.getProductById(4);
 
 console.log(productManager)
